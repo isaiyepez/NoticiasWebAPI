a1e758ab5320292ba2968f645819fcf960fb9c37
Isai Yepez Gómez <isaiyepez@gmail.com>
2019-04-09 13:30:48 -0500

    First commit

---
+++ NoticiasWebAPI/Models/Noticia.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace NoticiasWebAPI.Models
+{
+    public class Noticia
+    {
+        public int NoticiaID { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public string Contenido { get; set; }
+        public DateTime Fecha { get; set; }
+        public Autor Autor { get; set; }
+    }
+
+    public class Map
+    {
+        public Map(EntityTypeBuilder<Noticia> ebNoticia)
+        {
+                ebNoticia.HasKey(x => x.NoticiaID);
+                ebNoticia.Property(x => x.Titulo).HasColumnName("Titulo").HasMaxLength(50);
+                ebNoticia.Property(x => x.Descripcion).HasColumnName("Descripcion").HasMaxLength(100);
+                ebNoticia.Property(x => x.Contenido).HasColumnName("Contenido").HasMaxLength(int.MaxValue);
+                ebNoticia.Property(x => x.Fecha).HasColumnName("Fecha").HasColumnType("Datetime");
+                ebNoticia.HasOne(x => x.Autor).WithMany(x => x.Noticias).HasForeignKey(x => x.Autor);
+        }
+    }
+}
